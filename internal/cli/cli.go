729c6d4c0529e0c29d7c4afe6e9c67db1f4eacb9
reikanv <elv28299@gmail.com>
2024-01-22 09:28:20 +0300

    d

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -47,16 +47,16 @@ func ReadFlags() (xf XFlags, err error) {
 
 func row(iter int, x float64, pad int) string {
 	r, err := calc.Run(x)
-	paddedIter := strpad.Left(fmt.Sprintf("%v", iter), pad)
-	paddedX := strpad.Right(strpad.TrimFloat(fmt.Sprintf("%.5f", x)), pad)
+	paddedIter := strpad.Right(fmt.Sprintf("%v", iter), pad)
+	paddedX := strpad.Right(fmt.Sprintf("%.6f", x), pad)
 
 	if err != nil {
 		return fmt.Sprintf("%v | %v | ERROR", paddedIter, paddedX)
 	}
 
-	paddedR := strpad.Right(strpad.TrimFloat(fmt.Sprintf("%.5f", r)), pad)
+	paddedR := strpad.Right(fmt.Sprintf("%.6f", r), pad)
 
-	return fmt.Sprintf("%v | %v | %v", paddedIter, paddedX, paddedR)
+	return fmt.Sprintf("%v | %v | %v |", paddedIter, paddedX, paddedR)
 }
 
 func shouldRun(xf XFlags, x float64) bool {
@@ -71,13 +71,13 @@ func Out(xf XFlags) {
 	maxFlag := math.Max(xf.start, xf.end)
 	maxColLen := len(fmt.Sprintf("%v", maxFlag)) + 4
 	tableHead := fmt.Sprintf(
-		"%s | %s | %s",
-		strpad.Left("#", maxColLen),
+		"%s | %s | %s |",
+		strpad.Right("#", maxColLen),
 		strpad.Right("x", maxColLen),
 		strpad.Right("result", maxColLen),
 	)
 	fmt.Println(tableHead)
-	fmt.Println(strings.Repeat("—", len(tableHead)+1))
+	fmt.Println(strings.Repeat("—", len(tableHead)))
 
 	i := 1
 
